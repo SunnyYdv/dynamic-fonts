03bac88e88382e7873d44f12282aeaf18eb6bae1
Sunny Yadav <sunny@Sunnys-MacBook-Air-2.local>
2023-10-11 21:24:25 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <main className={'h-full w-full bg-zinc-950'}>
+      <div className={'flex h-full w-full items-center justify-center'}>
+        <Button>Test</Button>
+      </div>
+    </main>
+  )
+}
+
+export default App
